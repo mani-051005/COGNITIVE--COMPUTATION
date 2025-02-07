0b2ff8185c77759c0571468c23d4b9f7c6b08b4b
MANIKANTA <157005823+mani-051005@users.noreply.github.com>
2025-02-07 21:09:15 +0530

    Update Q1.py

--- ASSIGNMENT 5/Q1.py
+++ ASSIGNMENT 5/Q1.py
@@ -1,1 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
 
+# Q.1 For the array gfg = np.matrix([4, 1, 9; 12, 3, 1; 4, 5, 6]), Find
+gfg = np.matrix([[4, 1, 9], [12, 3, 1], [4, 5, 6]])
+
+# i. Sum of all elements
+sum_all_elements = np.sum(gfg)
+print("Sum of all elements:", sum_all_elements)
+
+# ii. Sum of all elements row-wise
+sum_row_wise = np.sum(gfg, axis=1)
+print("Sum of all elements row-wise:", sum_row_wise)
+
+# iii. Sum of all elements column-wise
+sum_column_wise = np.sum(gfg, axis=0)
+print("Sum of all elements column-wise:", sum_column_wise)
