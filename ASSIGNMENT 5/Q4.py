e1e05852a40dccefdb5411a110d2a50b0e6fff9b
MANIKANTA <157005823+mani-051005@users.noreply.github.com>
2025-02-07 21:14:48 +0530

    Update Q4.py

--- ASSIGNMENT 5/Q4.py
+++ ASSIGNMENT 5/Q4.py
@@ -1,1 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
 
+# Q4. Generate x values using np.linspace() from -10 to 10 with 100 points
+x = np.linspace(-10, 10, 100)
+
+
+y1 = x ** 2
+
+
+y2 = np.sin(x)
+
+
+y3 = np.exp(x)
+
+
+y4 = np.log(np.abs(x) + 1)
+
+
+plt.figure(figsize=(10, 6))
+
+plt.plot(x, y1, label="Y = x^2")
+
+plt.title("Plot of Y = x^2")
+plt.xlabel("X values")
+plt.ylabel("Y values")
+plt.grid(True)
+plt.legend()
+plt.show()
